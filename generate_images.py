f997bce31ed4e53ec5f91988520f819a3cc34bee
Prakhar Shukla <imprakharshukla@gmail.com>
2024-04-09 15:05:53 +0530

    change: images to json file

--- generate_images.py
+++ generate_images.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
-
 import asyncio
 import os
 import re
+import json
 
 import aiohttp
 
@@ -23,71 +22,40 @@ def generate_output_folder() -> None:
 
 
 ################################################################################
-# Individual Image Generation Functions
+# Individual JSON Generation Functions
 ################################################################################
 
 
 async def generate_overview(s: Stats) -> None:
     """
-    Generate an SVG badge with summary statistics
+    Generate summary statistics in JSON format
     :param s: Represents user's GitHub statistics
     """
-    with open("templates/overview.svg", "r") as f:
-        output = f.read()
-
-    output = re.sub("{{ name }}", await s.name, output)
-    output = re.sub("{{ stars }}", f"{await s.stargazers:,}", output)
-    output = re.sub("{{ forks }}", f"{await s.forks:,}", output)
-    output = re.sub("{{ contributions }}", f"{await s.total_contributions:,}", output)
-    changed = (await s.lines_changed)[0] + (await s.lines_changed)[1]
-    output = re.sub("{{ lines_changed }}", f"{changed:,}", output)
-    output = re.sub("{{ views }}", f"{await s.views:,}", output)
-    output = re.sub("{{ repos }}", f"{len(await s.repos):,}", output)
+    output = {
+        "name": await s.name,
+        "stars": await s.stargazers,
+        "forks": await s.forks,
+        "contributions": await s.total_contributions,
+        "lines_changed": sum(await s.lines_changed),
+        "views": await s.views,
+        "repos": len(await s.repos),
+    }
 
     generate_output_folder()
-    with open("generated/overview.svg", "w") as f:
-        f.write(output)
+    with open("generated/overview.json", "w") as f:
+        json.dump(output, f, indent=4)
 
 
 async def generate_languages(s: Stats) -> None:
     """
-    Generate an SVG badge with summary languages used
+    Generate summary languages used in JSON format
     :param s: Represents user's GitHub statistics
     """
-    with open("templates/languages.svg", "r") as f:
-        output = f.read()
-
-    progress = ""
-    lang_list = ""
-    sorted_languages = sorted(
-        (await s.languages).items(), reverse=True, key=lambda t: t[1].get("size")
-    )
-    delay_between = 150
-    for i, (lang, data) in enumerate(sorted_languages):
-        color = data.get("color")
-        color = color if color is not None else "#000000"
-        progress += (
-            f'<span style="background-color: {color};'
-            f'width: {data.get("prop", 0):0.3f}%;" '
-            f'class="progress-item"></span>'
-        )
-        lang_list += f"""
-<li style="animation-delay: {i * delay_between}ms;">
-<svg xmlns="http://www.w3.org/2000/svg" class="octicon" style="fill:{color};"
-viewBox="0 0 16 16" version="1.1" width="16" height="16"><path
-fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
-<span class="lang">{lang}</span>
-<span class="percent">{data.get("prop", 0):0.2f}%</span>
-</li>
-
-"""
-
-    output = re.sub(r"{{ progress }}", progress, output)
-    output = re.sub(r"{{ lang_list }}", lang_list, output)
+    output = await s.languages
 
     generate_output_folder()
-    with open("generated/languages.svg", "w") as f:
-        f.write(output)
+    with open("generated/languages.json", "w") as f:
+        json.dump(output, f, indent=4)
 
 
 ################################################################################
@@ -97,7 +65,7 @@ fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
 
 async def main() -> None:
     """
-    Generate all badges
+    Generate all JSON files
     """
     access_token = os.getenv("ACCESS_TOKEN")
     if not access_token:
@@ -117,8 +85,8 @@ async def main() -> None:
     # Convert a truthy value to a Boolean
     raw_ignore_forked_repos = os.getenv("EXCLUDE_FORKED_REPOS")
     ignore_forked_repos = (
-        not not raw_ignore_forked_repos
-        and raw_ignore_forked_repos.strip().lower() != "false"
+            not not raw_ignore_forked_repos
+            and raw_ignore_forked_repos.strip().lower() != "false"
     )
     async with aiohttp.ClientSession() as session:
         s = Stats(
